f173278b432173f5f958c8a12b0b0e45a3ef1303
heke1228 <chengkelfan@qq.com>
2025-11-26 17:18:08 +0800

    ...

--- uart_mmio.v
+++ uart_mmio.v
@@ -267,8 +267,7 @@ module uart_mmio #(
     always @(posedge clk) begin
         if (!resetn) begin
             mem_ready <= 0;
-        end
-        mem_ready <= mem_valid && !mem_instr;
+        end else mem_ready <= mem_valid && !mem_instr;
     end
 
     always @(posedge clk) begin: MMIO_READ
